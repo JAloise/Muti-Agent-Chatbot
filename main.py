c02e9603aee7bfe40f3ecbf593549304113a3986
Ivan <ivan.balkanski@protonmail.com>
2025-04-04 11:08:48 -0400

    feat: call ai

---
+++ main.py
@@ -0,0 +1,24 @@
+from agents.templates import controller_template
+from agents.agent import ControlAgent
+# models
+# ollama run llama3.2 / gemma3
+#response = ollama.list()
+#print(response)
+
+
+def main():
+    # create model
+    controller_llm = ControlAgent("llama3.2", controller_template)
+    print("*" * 100 + "\n" + "*" * 100) # visual line
+    print("Chatbot: Ask a question?(use q to quit)\n")
+    while True:
+        question = input("You: ")
+        if question.lower() == "q":
+            break
+        chatbot_response = controller_llm.answer_question(question)
+        print("Chatbot: " + chatbot_response)
+
+
+
+if __name__ == "__main__":
+    main()
