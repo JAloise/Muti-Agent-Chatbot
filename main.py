25c3684774ec49e2cbcd47e73eaa017dcc3a2e9b
Ivan <ivan.balkanski@protonmail.com>
2025-04-04 18:55:52 -0400

    chore: updated main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from agents.templates import controller_template
 from agents.agent import ControlAgent
+from constants import EMPTY_DATA
 # models
 # ollama run llama3.2 / gemma3
 #response = ollama.list()
@@ -8,7 +9,7 @@ from agents.agent import ControlAgent
 
 def main():
     # create model
-    controller_llm = ControlAgent("llama3.2", controller_template)
+    controller_llm = ControlAgent("llama3.2", controller_template,EMPTY_DATA)
     print("*" * 100 + "\n" + "*" * 100) # visual line
     print("Chatbot: Ask a question?(use q to quit)\n")
     while True:
