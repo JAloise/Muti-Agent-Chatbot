586954d774807bc2fa84579591fdad82a0db52d1
Ivan <ivan.balkanski@protonmail.com>
2025-04-05 16:29:58 -0400

    chore: controller data

--- constants.py
+++ constants.py
@@ -6,13 +6,18 @@ import os
 DIRECTORY_PATH = str(Path.cwd())
 
 # =================== data format ======================
-# *collection name *data location * embedded data location
+# *collection name *data location * embedded data location * data type
 EMPTY_DATA = [] # used for models with no extra path
 # concordia
 CONCORDIA_PATH = DIRECTORY_PATH + r"\data\concordia\\"
-CONCORDIA_DATA = ["concordia",CONCORDIA_PATH,CONCORDIA_PATH+r"db\\" ]
+CONCORDIA_DATA = ["concordia",CONCORDIA_PATH,CONCORDIA_PATH+r"db\\","pdf" ]
+# controller
+CONTROLLER_PATH = DIRECTORY_PATH + r"\data\controller\\"
+CONTROLLER_DATA = ["controller",CONTROLLER_PATH,CONTROLLER_PATH+r"db\\","csv" ]
 if os.system == "Darwin" or os.system == "Linux":
     CONCORDIA_PATH = DIRECTORY_PATH + r"/data/concordia//"
-    CONCORDIA_DATA = ["concordia", CONCORDIA_PATH, CONCORDIA_PATH + r"db//"]
+    CONCORDIA_DATA = ["concordia", CONCORDIA_PATH, CONCORDIA_PATH + r"db//","pdf"]
+    CONTROLLER_PATH = DIRECTORY_PATH + r"/data/controller//"
+    CONTROLLER_DATA = ["controller", CONTROLLER_PATH, CONTROLLER_PATH + r"db//", "csv"]
 
 
