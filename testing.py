0c676f42567f2697efd1eac2daf7552ba27aaf90
Ivan <ivan.balkanski@protonmail.com>
2025-04-06 16:28:57 -0400

    fix: fixed small typo

--- testing.py
+++ testing.py
@@ -9,7 +9,7 @@ questions = ["what is SOEN 423","can you give me a description of the material c
             "which classes in the computer science program cover embedded systems","which courses cover programing",
             "what is soen 390","what are its prerequisites","do I need to take the class to finish my computer science degree",
             "how many credits do i need to complete computer science","What is an LLM",
-            "Do i need math to use artifical intelligence","does concordia cover ai","why is the sky blue","I want to study in concordia, but I don't know how much a semester costs?"
+            "Do i need math to use artificial intelligence","does concordia cover ai","why is the sky blue","I want to study in concordia, but I don't know how much a semester costs?"
             ,"Are cars or trains faster?","WHO ARE YOU?","How can I reach a Concordia advisor","Can you compare MCgill and concordia","How much does a semester in Mcgill cost",
              "What are some examples of high paying jobs?","What is the longest bridge?","What happens if i combine bleach and ammonia?",
              "What will happen if i add water to the answer of the previous question","What is the definition of the word 'circle'?",
